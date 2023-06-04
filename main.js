e9c2bb4dd8e7ae66d608beef065b926fc903636b
Vilson <khvilson@yandex.ru>
2023-06-04 13:16:54 +0300

    fixed slider

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ $(function() {
     slidesToScroll: 1,
     infinit: true,
     arrows: true,
+    nextArrow: '<button class="next">→</button>',
+    prevArrow: '<button class="prev">←</button>',
+
   })
 
 })
